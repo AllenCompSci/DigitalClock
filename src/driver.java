c0c491f73c56e5d065369c18ab577a5a5ba9b6bf
allencompsci <Taylor_Hudson@allenisd.org>
2017-03-30 11:56:07 -0500

    d

--- src/driver.java
+++ src/driver.java
@@ -2,10 +2,12 @@ import generic.graphics.AudioPlayer;
 import generic.graphics.DigitalTimer;
 import generic.graphics.MasterClock;
 
+
 /**
  * Created by taylor hudson on 3/27/2017.
  */
 public class driver {
+
 
     public static void main(String [] args0){
         DigitalTimer time = new DigitalTimer();
